a59e681efdddb36678bc47747ecbccd7bf5392df
Enrique <octepc.97@gmail.com>
2022-09-07 17:36:23 -0500

    finished the work for today

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const keyboardLetters = [
 const listElements = [];
 let myAnswer = [];
 const secretWord = ["c", "o", "o", "k", "i", "e"];
+let positions = [];
 
 keyboardLetters.map((letters) => {
   const list = document.createElement("ul");
@@ -16,19 +17,19 @@ keyboardLetters.map((letters) => {
     switch (letter) {
       case "enter":
         listItem.innerHTML = `
-          <button onclick='checkWord()' id="letter">${letter}</button>
+          <button onclick='checkWord()' id="${letter}">${letter}</button>
         `;
         break;
 
       case "delete":
         listItem.innerHTML = `
-          <button onclick='deleteLetter()' id="letter">${letter}</button>
+          <button onclick='deleteLetter()' id="${letter}">${letter}</button>
         `;
         break;
 
       default:
         listItem.innerHTML = `
-          <button onclick='pressLetter()' id="letter">${letter}</button>
+          <button onclick='pressLetter()' id="${letter}">${letter}</button>
         `;
         break;
     }
@@ -41,14 +42,21 @@ keyboardLetters.map((letters) => {
 keyboard.append(...listElements);
 
 const checkWord = () => {
-  console.log("Revisando Palabra");
+  if (myAnswer.join("") === secretWord.join("")) {
+    console.log("Ganaste un oso de peluche");
+  } else {
+  }
 };
 
 const deleteLetter = () => {
-  console.log("Borrando letra");
+  myAnswer.pop();
+  console.log(myAnswer);
 };
 
 const pressLetter = () => {
   const button = event.target;
-  console.log(button.textContent);
+  if (myAnswer.length < 6) {
+    myAnswer.push(button.id);
+  }
+  console.log(myAnswer);
 };
